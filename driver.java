7a1709abd7a7a15895ceecba6de1bbc0499d5b27
tjhsst67 <1734653@fcpsschools.net>
2026-05-11 11:11:57 -0400

    Create driver.java
    Initial code added

---
+++ driver.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class driver
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Unit 5 Final Project");
+        frame.setSize(600, 900);
+        frame.setLocation(0, 0);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MainPanel p = new MainPanel();
+        frame.setContentPane(p);
+        p.requestFocus();
+        frame.setVisible(true);
+    }
+}
