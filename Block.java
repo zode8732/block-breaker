fb3a880a7170bca79ef0e88d269cbbcebaf1bfce
tjhsst67 <1734653@fcpsschools.net>
2026-05-13 10:44:54 -0400

    Update Block.java
    Initial code

--- Block.java
+++ Block.java
@@ -1,1 +1,22 @@
-// add code
+import java.awt.Color;
+import java.awt.*;
+
+public class Block {
+   
+    private int blockType = 0;
+    private int blockX = 0;
+    private int blockY = 0;
+   
+    public Block(int i, int j, int powerUpType) {
+        // creates block
+    }
+   
+    public void draw(int i, int j) {
+        // uses ball location to draw a rectangle
+    }
+
+    public int brokenBlock() {
+// makes sure to get rid of block
+return blockType;
+    }
+}
